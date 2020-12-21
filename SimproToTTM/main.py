2b1c4ee93d329b8e15d65c18893368ca3c3506e7
Razmo99 <lewis.munro@live.com>
2020-12-21 12:22:55 +1100

    New env SIMPRO_TARGET_COMPANIES

--- SimproToTTM/main.py
+++ SimproToTTM/main.py
@@ -8,6 +8,7 @@ import json
 import schedule
 from conf_mgr import conf_mgr
 import os, pathlib, sys
+from ast import literal_eval
 
 if getattr(sys,'frozen',False):
     #Change the current working directory to be the parent of the main.py
@@ -71,13 +72,37 @@ def main():
         logger.debug('Initialised Simpro Token Manager')
     else:
         exit()
-
+        
     if ttm_token.update_token() and simpro_token.update_token():
-        simpro_trackable_companies=[10]
+        #Grab all Companies specified by the user
+        simpro_selected_companies=[]
+        #Grab the Env var
+        SIMPRO_TARGET_COMPANIES=os.getenv('SIMPRO_TARGET_COMPANIES')
+        #If its not an empty string or none use literal eval on it
+        simpro_target_companies=literal_eval(SIMPRO_TARGET_COMPANIES) if SIMPRO_TARGET_COMPANIES else None
+        #Grab all companies in the simpro tenant to match against
+        with SimproAPI.Sessions(simpro_token.server,simpro_token.access_token) as session:
+            simpro_companies=session.companies_get_all().json()
+        #If "SIMPRO_TARGET_COMPANIES" are provided start matching
+        if simpro_target_companies:
+            #Iterate over the retrieved tenant companies
+            for simpro_company in simpro_companies:
+                #Iterate over each companies keys
+                for (key,value) in simpro_company.items():
+                    #Iterate over each selected target companies
+                    for simpro_target_company in simpro_target_companies:
+                        # try to march the target to a known company in the simpro tenant, using lower case string match
+                        # Doesn't append duplicate matches
+                        if str(value).lower() == str(simpro_target_company).lower() and simpro_company['ID'] not in [i['ID'] for i in simpro_selected_companies]:
+                            simpro_selected_companies.append(simpro_company)
+        #Else Target all companies in the simpro tenant
+        else:
+            simpro_selected_companies=[i['ID'] for i in simpro_companies]
+        
         #List containing information on companies that have trackable plants.
         with SimproAPI.Trackables(simpro_token.server,simpro_token.access_token) as trackables:
             simpro_trackable_companies=trackables.get_companies(
-                simpro_trackable_companies,
+                simpro_selected_companies,
                 [conf.config['simpro_tracker']['serial'],conf.config['simpro_tracker']['last_known_location']],
                 concurrently=True
             )
@@ -99,7 +124,7 @@ def main():
                     compare_data=SimproAPI.Trackables(simpro_token.server,simpro_token.access_token).compare_equipment(
                         company['id'], # id of the company
                         trackable_plant['id'], # id of the trackable plant
-                        trackable_plant['trackable_plant'], # Plant equipment to match againts
+                        trackable_plant['trackable_plant'], # Plant equipment to match against
                         ttm_devices, # List of ttm devices to match against
                         'serialNumber', #Serial Number field in ttm
                         ['serialNumber','lastLatitude','lastLongitude'], # other ttm fields to return when a match is found
